080b8265ecca56d839a0a52556b262393c621a0c
Yersain Aldabayev <ersainaldabaev@gmail.com>
2022-01-02 23:56:42 +0100

    start

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
+
+import './App.css';
+import './App.scss';
+
+
+
+
+const App = () => {
+  return (
+    <div className='app-wrapper'>
+      <Header />
+      <Navbar />
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://cdn.pixabay.com/photo/2018/03/28/04/02/logo-3268177_1280.png" alt="logo"/>
+        </header>
+    )
+}
+
+export default Header;
