1d48066aab49fa166c22352115282615a2d3b287
Yersain Aldabayev <ersainaldabaev@gmail.com>
2022-01-04 23:14:09 +0100

    feat: react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
 import './App.css';
-import Dialogs from './components/Dialogs/Dialogs';
+
 
 
 
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <div className='app-wrapper-content'>
-        <Dialogs />
-        {/* <Profile /> */}
-      </div>
-      
-    </div>
+      <BrowserRouter>
+        <div className='app-wrapper'>
+          <Header />
+          <Navbar />
+          <div className='app-wrapper-content'>
+            <Routes>
+              <Route path='/profile'  element={<Profile />} />
+              <Route path='/dialogs' element={<Dialogs />} />
+            </Routes>
+          </div>
+          
+        </div>
+      </BrowserRouter>
   );
 }
 
