8256c99b6d7d67e0368e048cd2afd35404579ee3
Yersain Aldabayev <ersainaldabaev@gmail.com>
2022-02-14 00:12:06 +0100

    fix: refactoring from dumb component to class component (class practice)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,48 +4,47 @@ import * as axios from 'axios';
 import userPhoto from '../../assets/images/user.png';
 
 
-
-
-const Users = (props) => {
-
-  const getUsers = () => {
-    if (props.users.length === 0) {
-      axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-        props.setUsers(response.data.items);
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+        this.props.setUsers(response.data.items);
       });
-    }
+    
   }
 
-  return <div className={styles.wrapper}>
-    <button onClick={getUsers}>Get Users</button>
-    
-    {
-      props.users.map(user => <div className={styles.userWrapper} key={user.id}>
-        <span>
-          <div>
-            <img src={user.photos.small != null ? user.photos.small : userPhoto} className={styles.userPhoto} alt="#" />
-          </div>
-          <div>
-            <button className={styles.followBtn} onClick={() => props.followToggle(user.id)}>
-              {user.followed ? 'Unfollow' : 'Follow'}
-            </button>
-          </div>
-        </span>
-        <span className={styles.userDescription}>
-          <span>
-            <div className={styles.userName}>{user.name}</div>
-            <div>{user.status}</div>
-          </span>
-          <span className={styles.userCountry}>
-            <div>{'user.location.country'}</div>
-            <div>{'user.location.city'}</div>
-          </span>
 
-        </span>
-      </div>
-      )
-    }
-  </div>
+  render() {
+    return <div className={styles.wrapper}>
+
+      {
+        this.props.users.map(user => <div className={styles.userWrapper} key={user.id}>
+          <span>
+            <div>
+              <img src={user.photos.small != null ? user.photos.small : userPhoto} className={styles.userPhoto} alt="#" />
+            </div>
+            <div>
+              <button className={styles.followBtn} onClick={() => this.props.followToggle(user.id)}>
+                {user.followed ? 'Unfollow' : 'Follow'}
+              </button>
+            </div>
+          </span>
+          <span className={styles.userDescription}>
+            <span>
+              <div className={styles.userName}>{user.name}</div>
+              <div>{user.status}</div>
+            </span>
+            <span className={styles.userCountry}>
+              <div>{'user.location.country'}</div>
+              <div>{'user.location.city'}</div>
+            </span>
+
+          </span>
+        </div>
+        )
+      }
+    </div>
+  }
 }
 
 export default Users;
