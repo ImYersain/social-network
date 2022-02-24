91c6235b8e7f3e176a14944864abd00210e61798
Yersain Aldabayev <ersainaldabaev@gmail.com>
2022-02-24 21:18:48 +0100

    fix: refactoring users component(preloader and disable users) + changed styles

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -9,52 +9,53 @@ import styles from './Users.module.css';
 const Users = (props) => {
 
   let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
-    let pages = [];
+  let pages = [];
 
-    for (let i = 1; i <= pagesCount; i++) {
-      if (pages.length < 10) {
-        pages.push(i);
-      }
+  for (let i = 1; i <= pagesCount; i++) {
+    if (pages.length < 10) {
+      pages.push(i);
     }
+  }
+
+  let user = props.users.map(user => <div className={styles.userWrapper} key={user.id}>
+    <span>
+      <div>
+        <img src={user.photos.small != null ? user.photos.small : userPhoto} className={styles.userPhoto} alt="#" />
+      </div>
+      <div>
+        <button className={styles.followBtn} onClick={() => props.followToggle(user.id)}>
+          {user.followed ? 'Unfollow' : 'Follow'}
+        </button>
+      </div>
+    </span>
+    <span className={styles.userDescription}>
+      <span>
+        <div className={styles.userName}>{user.name}</div>
+        <div>{user.status}</div>
+      </span>
+      <span className={styles.userCountry}>
+        <div>{'user.location.country'}</div>
+        <div>{'user.location.city'}</div>
+      </span>
+    </span>
+  </div>
+  ) 
+  
+
 
 
   return <>
-  
-  <div className={styles.wrapper}>
-  {props.isFetching ? <Preloader />:null}
-  {
-    props.users.map(user => <div className={styles.userWrapper} key={user.id}>
-      <span>
-        <div>
-          <img src={user.photos.small != null ? user.photos.small : userPhoto} className={styles.userPhoto} alt="#" />
-        </div>
-        <div>
-          <button className={styles.followBtn} onClick={() => props.followToggle(user.id)}>
-            {user.followed ? 'Unfollow' : 'Follow'}
-          </button>
-        </div>
-      </span>
-      <span className={styles.userDescription}>
-        <span>
-          <div className={styles.userName}>{user.name}</div>
-          <div>{user.status}</div>
-        </span>
-        <span className={styles.userCountry}>
-          <div>{'user.location.country'}</div>
-          <div>{'user.location.city'}</div>
-        </span>
 
-      </span>
+    <div className={styles.pages}>
+      {pages.map(page => {
+        return <span onClick={(e) => { props.onPageChanged(page) }} className={props.currentPage === page ? styles.selectedPage : null}>{page}</span>
+      })}
     </div>
-    )
-  }
-</div>
-<div className={styles.pages}>
-    {pages.map(page => {
-      return <span onClick={(e) => { props.onPageChanged(page) }} className={props.currentPage === page ? styles.selectedPage : null}>{page}</span>
-    })}
-  </div>
-</>
+    <div className={styles.wrapper}>
+      {props.isFetching ? <Preloader style={{ width: '50%', margin: '0 auto' }} /> : user}
+    </div>
+    
+  </>
 }
 
 
