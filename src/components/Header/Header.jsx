264df8bbeb7b42fc70b67432be37ac8eb8b4c888
Yersain Aldabayev <ersainaldabaev@gmail.com>
2022-03-07 00:04:49 +0100

    feat: added login image (login-avatar)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,20 @@
 import React from 'react'; 
 import {NavLink} from 'react-router-dom'
+import loginAvatar from '../../assets/images/login-avatar.png';
 
 import s from './Header.module.css';
 
 const Header = (props) => {
-    return (
+    let login = (<>
+        <img style={{'width':'40px', 'height':'40px'}} src={loginAvatar}></img>
+        <NavLink to={'/login'}>login</NavLink>        
+    </>)
+
+    return ( 
         <header className={s.header}>
             <img src="https://cdn.pixabay.com/photo/2018/03/28/04/02/logo-3268177_1280.png" alt="logo"/>
             <div className={s.loginBlock}>
-                {props.isAuth ? props.login: <NavLink to={'/login'}>login</NavLink>}
+                {props.isAuth ? props.login: login}
             </div>
         </header>
     )
