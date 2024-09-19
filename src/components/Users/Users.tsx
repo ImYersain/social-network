6695417f0e1e9c3ce33659dc100e1450ba5a979e
yersain.aldabayev <>
2024-09-20 00:26:47 +0200

    feat: work with useNavigate, useLocation, added queryString lib

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -10,6 +10,8 @@ import {UsersSearchForm} from './UsersSearchForm';
 import {requestUsers, UserFilterType} from '../../Redux/users-reducer';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCurrentPage, getFollowingInProgress, getIsFetching, getPageSize, getTotalUsersCount, getUserSelector, getUsersFilter } from '../../Redux/users-selectors';
+import { useLocation, useNavigate } from 'react-router-dom';
+import queryString from 'query-string';
 
 type PropsType = {
 };
@@ -24,6 +26,8 @@ export const Users: FC<PropsType> = (props) => {
   const filter = useSelector(getUsersFilter);
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
 
   const onPageChanged = (pageNumber: number) => {
     dispatch(requestUsers(pageNumber, pageSize, filter));
@@ -34,9 +38,33 @@ export const Users: FC<PropsType> = (props) => {
   };
 
   useEffect(() => {
-    dispatch(requestUsers(currentPage, pageSize, {term: '', friend: ''}));
+    // чтобы из урл доставать куери параметры если они есть, и при первой инициализации записывать в редакс (в наше хранилище состояний) чтобы на странице отрисовалось
+    const parsedQuery: { [key: string]: any } = queryString.parse(location.search.substring(1));
+    let actualPage = currentPage;
+    let actualTerm = '';
+    let actualFriend = '';
+    
+    if(!!parsedQuery.page) actualPage = parseInt(parsedQuery.page);
+    if(!!parsedQuery.term) actualTerm = parsedQuery.term;
+    if(!!parsedQuery.friend) actualFriend = parsedQuery.friend;
+
+    dispatch(requestUsers(actualPage, pageSize, {term: actualTerm, friend: actualFriend}));
   }, []);
 
+  // синхранизация урл и редакс хранилища (если нажал на поиске фильтр, то он будет в урл)
+  useEffect(() => {
+    let query:  { [key: string]: any } = {};
+
+    if(filter.term) query.term = filter.term;
+    if(filter.friend) query.friend = filter.friend;
+    if(currentPage > 1) query.page = currentPage;
+
+    navigate({
+      pathname: '/users',
+      search: queryString.stringify(query),
+      //search: `?term=${filter.term}&friend=${filter.friend}&page=${currentPage}`,   first easiest version 
+    });
+  }, [filter.term, filter.friend, currentPage]);
 
   return (
     <>
